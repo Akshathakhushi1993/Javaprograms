b0273546efd4119b67b50f47880c1e1ce07d8fb4
akshatha <akshatha.a@itvedant.com>
2024-01-27 21:09:00 +0530

    first commit

---
+++ Student.java
@@ -0,0 +1,14 @@
+class Student
+{
+public static void main(String[] args){
+//This is variable Decaration
+int age;
+//This is Variable Initialization
+age=20;
+//This is variable initialization
+age=20;
+String name="Jerry";
+System.out.println("the age of a student is ="+age);
+System.out.println("the name of the student is ="+name);
+}
+}
