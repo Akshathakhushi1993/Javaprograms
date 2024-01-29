4b4fcf2dce5a965a032d8924c769f464a3545b30
Akshathakhushi1993 <106955081+Akshathakhushi1993@users.noreply.github.com>
2024-01-29 21:13:13 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student
+{
+//NON-STATIC VARIABLES
+int age=20;
+String name='Dinga';
+
+public static void main(string[] args)
+{
+System.out.println("start");
+Student s=new Student();
+System.out.println(s.age);
+
+System.out.println(s.name);
+
+System.out.println("end");
+}
+}
